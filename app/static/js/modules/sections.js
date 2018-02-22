daf38e075602e742359330f5527e6028e7b2c23e
James MacBook Pro <james.p.perrone@gmail.com>
2018-02-22 15:32:27 +0100

    Loader Test

--- app/static/js/modules/sections.js
+++ app/static/js/modules/sections.js
@@ -15,7 +15,7 @@ const sections = {
     // Make sure the website is always scrolled to the top before rendering
     // a new page.
     window.scrollTo(0, 0)
-    window.onload = () => {
+    window.onload = function () {
       console.log('test')
       helpers.loader.hide()
     }
